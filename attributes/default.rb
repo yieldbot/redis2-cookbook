5233865da580f3978d948c97c652ca58eff66da9
Dave White <dwhite@yieldbot.com>
2013-05-01 08:28:25 -0400

    make more things configurable in redis.con

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,20 @@ default["redis2"]["conf_dir"] = "/etc/redis"
 default["redis2"]["pid_dir"] = "/var/run/redis"
 default["redis2"]["user"] =     "redis"
 default["redis2"]["deamon"] = "redis-server"
+default["redis2"]["instances"]["default"]["daemonize"] = "no"
 default["redis2"]["instances"]["default"]["bindaddress"] = "0.0.0.0"
 default["redis2"]["instances"]["default"]["port"] = "6379"
 default["redis2"]["instances"]["default"]["timeout"] = 300
+default["redis2"]["instances"]["default"]["loglevel"] = "notice"
+default["redis2"]["instances"]["default"]["syslog-enabled"] = "no"
+default["redis2"]["instances"]["default"]["syslog-ident"] = "redis"
+default["redis2"]["instances"]["default"]["syslog-facility"] = "USER"
+default["redis2"]["instances"]["default"]["bgsave"] = "900 1"
+default["redis2"]["instances"]["default"]["rdbcompression"] = "yes"
 default["redis2"]["instances"]["default"]["dumpdb_filename"] = "dump.rdb"
 default["redis2"]["instances"]["default"]["data_dir"] = "/var/lib/redis"
+default["redis2"]["instances"]["default"]["slave-serve-stale-data"] = "yes"
+default["redis2"]["instances"]["default"]["slave-read-only"] = "no"
 default["redis2"]["instances"]["default"]["activerehashing"] = "yes" # no to disable, yes to enable
 default["redis2"]["instances"]["default"]["databases"] = 16
 
@@ -16,13 +25,5 @@ default["redis2"]["instances"]["default"]["appendonly"] = "no"
 default["redis2"]["instances"]["default"]["appendfsync"] = "everysec"
 default["redis2"]["instances"]["default"]["no_appendfsync_on_rewrite"] = "no"
 
-default["redis2"]["instances"]["default"]["vm"]["enabled"] = "no" # no to disable, yes to enable
-default["redis2"]["instances"]["default"]["vm"]["swap_file"] = "/var/lib/redis/swap"
-default["redis2"]["instances"]["default"]["vm"]["max_memory"] = node["memory"]["total"].to_i * 1024 * 0.7
-default["redis2"]["instances"]["default"]["vm"]["page_size"] = 32 # bytes
-default["redis2"]["instances"]["default"]["vm"]["pages"] = 134217728 # swap file size is pages * page_size
-default["redis2"]["instances"]["default"]["vm"]["max_threads"] = 4
-
 default["redis2"]["instances"]["default"]["maxmemory_samples"] = 3
 default["redis2"]["instances"]["default"]["maxmemory_policy"] = "volatile-lru"
-default["redis2"]["instances"]["default"]["bgsave"] = true
